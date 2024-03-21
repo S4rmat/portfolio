3ea28e9198bf83e90ff2338c9c93be43b362f6ba
S4rmat <156938561+S4rmat@users.noreply.github.com>
2024-03-21 16:15:56 +0300

    Add files via upload

---
+++ MyPos/pos_restaurant/conn_db.java
@@ -0,0 +1,30 @@
+package pos_restaurant;
+
+
+/**
+ *
+ * @author Admin
+ */
+import java.sql.*;
+import javax.swing.*;
+
+public class conn_db {
+    
+    Connection conn=null;
+    public static Connection conn_jdb(){
+        
+        try{
+            Class.forName("org.sqlite.JDBC");
+            Connection conn =DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Admin\\Downloads\\sqlite\\dbases\\restpos.sqlite");
+            //JOptionPane.showMessageDialog(null, "Connection to database is successful");
+            
+            return conn;
+           
+            
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+        
+    }
+}
---
+++ MyPos/pos_restaurant/user.java
@@ -0,0 +1,19 @@
+package pos_restaurant;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author S4rmat
+ */
+public class user {
+    public static int emp_id;
+   
+   
+    
+}
